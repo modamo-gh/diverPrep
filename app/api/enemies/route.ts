0f986b8ef7523b0ad1d4e1c49b56e0be8844550e
modamo-gh <morganthemosaic@gmail.com>
2025-02-01 13:58:57 -0500

    Add API route to retrieve enemies from db

---
+++ app/api/enemies/route.ts
@@ -0,0 +1,32 @@
+import * as dotenv from "dotenv";
+import { NextResponse } from "next/server";
+import { Client } from "pg";
+
+dotenv.config();
+
+const client = new Client({
+	host: process.env.PGHOST,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD,
+	database: process.env.PGDATABASE,
+	port: Number(process.env.PGPORT)
+});
+
+export const GET = async () => {
+	try {
+		await client.connect();
+
+		const result = await client.query("SELECT * FROM enemies;");
+
+		await client.end();
+
+		return NextResponse.json(result.rows);
+	} catch (error) {
+		console.error("Error fetching enemies:", error);
+
+		return NextResponse.json(
+			{ error: "Failed to fetch data" },
+			{ status: 500 }
+		);
+	}
+};
