8207f30490f4f996b6123a2346ad55205339a7c3
modamo-gh <morganthemosaic@gmail.com>
2025-02-03 16:34:36 -0500

    Apply grid to tactical assessment

--- components/TacticalAssessment.tsx
+++ components/TacticalAssessment.tsx
@@ -7,27 +7,25 @@ const TacticalAssessment: React.FC<TacticalAssessmentProps> = ({
 	weapons
 }) => {
 	return (
-		<div className="bg-gray-800 border border-gray-600 grid grid-cols-3 items-center justify-around rounded text-lg">
-			<div className="flex flex-col gap-1 items-center justify-around">
-				<p className="text-center">Weighted Armor Value?</p>
-				<p>
+		<div className="bg-gray-800 border border-gray-600 flex flex-col items-center justify-around rounded text-lg">
+			<div className="flex-1 grid grid-cols-3 items-center justify-center w-full">
+				<p className="text-center">Weighted AV?</p>{" "}
+				<p className="text-center">Most Common AV?</p>{" "}
+				<p className="text-center">Max AV?</p>
+			</div>
+			<div className="flex-1 grid grid-cols-3 items-center justify-center w-full">
+				<p className="text-center">
 					{weapons[weaponIndex]?.penetration >=
 					filteredEnemies[enemyIndex]?.weightedaverage
 						? "✅"
 						: "❌"}
 				</p>
-			</div>
-			<div className="flex flex-col gap-1 items-center justify-around">
-				<p className="text-center">Most Common Armor Value?</p>
 				<p className="text-center">
 					{weapons[weaponIndex]?.penetration >=
 					filteredEnemies[enemyIndex]?.mode
 						? "✅"
 						: "❌"}
 				</p>
-			</div>
-			<div className="flex flex-col gap-1 items-center justify-around">
-				<p className="text-center">Max Armor Value?</p>
 				<p className="text-center">
 					{weapons[weaponIndex]?.penetration >=
 					filteredEnemies[enemyIndex]?.max
