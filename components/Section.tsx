0a7ed80086e271d949773afbbdeec12857886b2b
modamo-gh <morganthemosaic@gmail.com>
2025-02-04 12:57:40 -0500

    Prevent section expansion on md layouts and up

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,5 +1,6 @@
 import { SectionProps } from "@/types/props/SectionProps";
 import SectionHeader from "./SectionHeader";
+import useIsSmall from "@/hooks/useIsSmall";
 
 const Section: React.FC<SectionProps> = ({
 	children,
@@ -7,14 +8,15 @@ const Section: React.FC<SectionProps> = ({
 	name,
 	toggleSection
 }) => {
-	const isExpanded = expandedSection === name;
+	const isSmall = useIsSmall();
+	const isExpanded = expandedSection === name && isSmall;
 
 	return (
 		<div
 			className={`flex flex-col gap-2 ${
 				isExpanded ? "flex-[8]" : "flex-[1]"
 			} transition-all duration-300 ease-in-out`}
-			onClick={() => toggleSection(name)}
+			onClick={() => isSmall && toggleSection(name)}
 		>
 			{!isExpanded && <SectionHeader name={name} />}
 			{children}
